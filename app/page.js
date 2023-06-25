7f4567e9d5315728e7dc78430b8f22305e224ef2
nanak <sidhu.nds22@gmail.com>
2023-06-25 23:17:37 +0530

    preload fixed

--- app/page.js
+++ app/page.js
@@ -8,55 +8,67 @@ export default function Home() {
   const scrollRef=useRef()
   const contextRef=useRef()
   const [loading,setLoading]=useState(false)
-
-  const getImageName = (index) =>
-  `./src/assets/${index.toString().padStart(4, "0")}.jpg`;
-
-const preloadImages = () => {
-  const imageLoadedPromises = [];
-  for (let i = 1; i < totalFrames; i++) {
-    const img = new Image();
-    const imgLoadPromise = new Promise((resolve, reject) => {
-      img.onload = () => resolve(img);
-      img.onerror = reject("Something is wrong...");
-    });
-    imageLoadedPromises.push(imgLoadPromise);
-    img.src = getImageName(i);
-  }
-  return Promise.all(imageLoadedPromises);
-};
-
-const updateImage = (index) => {
-  img.src = getImageName(index);
-  contextRef.current.drawImage(
-    img,
-    0,
-    0,
-    canvasRef.current.width,
-    canvasRef.current.height
-  );
-};
-
+  const imagesRef=useRef()
+  const totalFrames=148
+  
   const { scrollYProgress } = useScroll();
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {
-    contextRef.current=canvasRef?.current?.getContext("2d")
-    scrollRef.current=latest*149-latest*149%1
-    updateImage(scrollRef.current)
-  })
+
+  const preloadImages = () => {
+    setLoading(true)
+    const imageLoadedPromises = [];
+    for (let i = 1; i < totalFrames; i++) {
+      const img = new Image();
+      const imgLoadPromise = new Promise((resolve, reject) => {
+        img.onload = () => {resolve(img)
+        // console.log(i,"Succ")
+        };
+        img.onerror =()=>{ reject("Something is wrong...");
+        // console.log(i,"Error")
+      }
+      });
+      imageLoadedPromises.push(imgLoadPromise);
+      img.src = getImageName(i);
+    }
+    return Promise.all(imageLoadedPromises);
+  };
 
   
-  useEffect(()=>{
-    setLoading(true)
-    preloadImages()
-    setLoading(false)
-  },[])
+  const getImageName = (index) =>
+  "assets/"+index.toString().padStart(4, "0")+".jpg";
 
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    contextRef.current=canvasRef?.current?.getContext("2d")
+    scrollRef.current=latest*148-latest*148%1+1
+    const img= new Image()
+
+    const updateImage = (index) => {
+      img.src = getImageName(index);
+      contextRef.current.drawImage(
+        img,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+    };
+    
+    updateImage(scrollRef.current)
+
+  })
+
+  useEffect(()=>{
+    const images=preloadImages()
+    images.then((val)=>{
+      imagesRef.current=val 
+      setLoading(false)},setLoading(false))
+  },[])
 
   return (
     <>
 
-    <h1 className='text-8xl'> HII</h1>
-{loading?"Loading Please Wait":    <div className="canvasContainer sticky top-0 h-screen">
+    <h1 className='text-8xl'> Its Parkour time</h1>
+{loading?"Please wait":  <div className="canvasContainer sticky top-0 h-screen">
     <canvas ref={canvasRef} className="relative w-full h-screen bg-teal-300 z-10"></canvas>
     {scrollRef.current<=133?<div classname="z-20 absolute flex justify-center items-center backdrop-blur-lg bg-black/30">
       <h1 className="text-8xl text-white">Parkour </h1>
